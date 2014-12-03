e9c56ef210bd576ab99d3b1f4f093145a76787dd
Najib EB <najib1@gmail.com>
2014-12-03 01:04:13 +0000

    Initial commit

---
+++ app/view1/view1.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute', 'ui.grid'])
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
+
+    .controller('View1Ctrl', ['$scope', function ($scope) {
+        $scope.myData = [
+            {
+                "firstName": "Cox",
+                "lastName": "Carney",
+                "company": "Enormo",
+                "employed": true
+            },
+            {
+                "firstName": "Lorraine",
+                "lastName": "Wise",
+                "company": "Comveyer",
+                "employed": false
+            },
+            {
+                "firstName": "Nancy",
+                "lastName": "Waters",
+                "company": "Fuelton",
+                "employed": false
+            }];
+    }]);
