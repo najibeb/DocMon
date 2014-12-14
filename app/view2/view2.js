59f92b6b482ae30f41cf47202e0c1600098d955c
Najib EB <najib1@gmail.com>
2014-12-14 17:52:43 +0000

    smart tables

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,22 +1,22 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute','smart-table'])
+angular.module('myApp.view2', ['ngRoute', 'smart-table'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: 'view2/view2.html',
+            controller: 'View2Ctrl'
+        });
+    }])
 
-.controller('View2Ctrl', [ '$scope', '$http',function($scope, $http) {
-      $scope.itemsByPage=5;
+    .controller('View2Ctrl', ['$scope', '$http', function ($scope, $http) {
+        $scope.itemsByPage = 5;
 
-      $http.get("/DocMon/app/test_data").success(function (data, status, headers, config) {
-            $scope.rowCollection   = data;
-          }
-      );
+        $http.get("/DocMon/app/test_data").success(function (data, status, headers, config) {
+                $scope.rowCollection = data;
+            }
+        );
 
-      $scope.displayed = [].concat($scope.rowCollection);
+        $scope.displayed = [].concat($scope.rowCollection);
 
     }]);
